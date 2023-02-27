ef86ab92525c36fd0483cdd20fa2e404b9fa8906
sweauxui <125528967+sweauxui@users.noreply.github.com>
2023-02-27 16:13:53 +0900

    Add files via upload

---
+++ 01Minecraft.js
@@ -0,0 +1,105 @@
+var swiper1 = new Swiper(".gamecard", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  keyboard: true,
+  autoplay: {
+    delay: 3200,
+    disableOnInteraction: false
+  },
+});
+
+var swiper2 = new Swiper(".News", {
+  slidesPerView: 2.5,
+  spaceBetween: 40,
+  breakpoints: {
+    899: {
+      slidesPerView: 2.5,
+      spaceBetween: 80,
+    },
+    1199: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    },
+    1439: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    },
+  },
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper3 = new Swiper(".pickcard", {
+  spaceBetween: 40,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(document).ready(function () {
+  $('.Languages').click(function () {
+    if (!$('.gloval').hasClass('on')) { //만약 글로벌이 온을 가지고 있지 않으면
+      $('.gloval').addClass('on');//온을 추가해라
+    }
+    else {
+      $('.gloval').removeClass('on'); //가지고 있으면 온을 제거해라
+    }
+  });
+
+  // 햄버거 버튼 클릭하면 gnb 나타남, 로그인 스타트 gnb안에 들어감
+  $('.hamburger').click(function () {
+
+    if (!$('.gnbfamily').hasClass('on')) {
+      $('.gnbfamily').addClass('on');
+    }
+    else {
+      $('.gnbfamily').removeClass('on');
+    }
+
+    if (!$('.login_start').hasClass('on')) {
+      $('.login_start').addClass('on');
+    }
+    else {
+      $('.login_start').removeClass('on');
+    }
+
+    $('.login_start').appendTo('.gnbfamily');
+
+    $('.gnbfamily .sub li').removeClass('on');
+
+    $(this).toggleClass('on');
+  });
+
+  // 
+
+  $('.gnbfamily li button').click(function () {
+    if (!$('.gnbfamily .sub li').hasClass('on')) {
+      $('.gnbfamily .sub li').addClass('on');
+    }
+    else {
+      $('.gnbfamily .sub li').removeClass('on');
+    }
+  });
+})
+
+$(window).resize(function(){
+  location.reload();
+})
